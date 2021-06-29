c1466b6c2ec7ff7d12998aa5dad57f1d4e94bf31
syaikhul-anam <syaikhulanam@mhs.unida.gontor.ac.id>
2021-06-29 19:33:44 +0700

    + Detail Ticket

---
+++ app/src/main/java/com/example/mytiket/TicketCheckoutActivity.java
@@ -0,0 +1,14 @@
+package com.example.mytiket;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TicketCheckoutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ticket_checkout);
+    }
+}
--- app/src/main/java/com/example/mytiket/TicketDetailActivity.java
+++ app/src/main/java/com/example/mytiket/TicketDetailActivity.java
@@ -2,13 +2,28 @@ package com.example.mytiket;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class TicketDetailActivity extends AppCompatActivity {
-
+     Button btn_buy_ticket;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ticket_detail);
+
+        btn_buy_ticket = findViewById(R.id.btn_buy_ticket);
+
+        btn_buy_ticket.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent gotocheckout = new Intent(TicketDetailActivity.this, TicketCheckoutActivity.class);
+                startActivity(gotocheckout);
+            }
+        });
+
+
     }
 }
