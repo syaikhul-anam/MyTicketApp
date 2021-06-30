769f9d39d706ae70bc039250e6a2ed9ab8cbf78f
syaikhul-anam <syaikhulanam@mhs.unida.gontor.ac.id>
2021-06-30 09:48:24 +0700

    + Succes buy ticket

--- app/src/main/java/com/example/mytiket/TicketCheckoutActivity.java
+++ app/src/main/java/com/example/mytiket/TicketCheckoutActivity.java
@@ -2,13 +2,25 @@ package com.example.mytiket;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class TicketCheckoutActivity extends AppCompatActivity {
-
+     Button btn_buy_ticket;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ticket_checkout);
+
+        btn_buy_ticket = findViewById(R.id.btn_buy_ticket);
+        btn_buy_ticket.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent gotosuccesticket = new Intent(TicketCheckoutActivity.this,SuccesBuyTicketActivity.class);
+                startActivity(gotosuccesticket);
+            }
+        });
     }
 }
