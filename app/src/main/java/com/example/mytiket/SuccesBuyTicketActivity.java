eebe69a036bcd4984c9874ac2f49784196edfa2f
syaikhul-anam <syaikhulanam@mhs.unida.gontor.ac.id>
2021-06-30 17:06:45 +0700

    + Ticket Details

---
+++ app/src/main/java/com/example/mytiket/SuccesBuyTicketActivity.java
@@ -0,0 +1,14 @@
+package com.example.mytiket;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SuccesBuyTicketActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_succes_buy_ticket_activity);
+    }
+}
